0e59c3d98ac8a795fbc21069bdf03e4249978f0a
yashdev0000 <ysshu0000@gmail.com>
2024-08-14 12:07:35 +0530

    Add files via upload

---
+++ doctype.py
@@ -0,0 +1,16 @@
+<doctype html>
+<Html>
+<Head>
+<Body>
+<Button>
+<Span>button</button>
+</Button>
+<Button>
+<Span>button</button>
+</Button>
+<Button>
+<Span>button</button>
+</Button>
+</Head>
+</Body>
+</Html>
